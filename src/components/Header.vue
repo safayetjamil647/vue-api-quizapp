15466d688de99457e8d115e93334561fa05e933b
safayetjamil647 <work.jamil4@gmail.com>
2020-05-29 03:43:00 +0600

    v0.05

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled>
+        <b>Fancy quiz</b>
+      </b-nav-item>
+      <b-nav-item disabled>Counter</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <b-jumbotron>
+      <template v-slot:lead>{{ currentQuestion.question }}</template>
+
+      <hr class="my-4" />
+
+      <p>Answer</p>
+
+      <b-button variant="primary" href="#">Do Something</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function,
+  },
+};
+</script>
+>
